b59509be699de148a6b124bf669b5a407cbcfb28
nextjs-iot-third-appgit add .git commit -m maingit branch -m maingit push -u origin maingit config --global user.name next-iot-third-app <phijadev@gmail.com>
2026-02-02 15:56:27 +0700

    edite

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,8 @@ export default function Footer() {
   return (
     <>
     <hr className="mb-2"/>
-    <h3 className="text-center text-sm from-blue-500 to-orange-800 bg-linear-to-r bg-clip-text text-transparent font-bold mt-2 mb-4">
+    <h3 className="text-center text-sm from-blue-700
+        to-orange-900 bg-linear-to-r bg-clip-text text-transparent font-bold     mt-2 mb-4">
         &copy; 2026 Dev Phimaha Coding School IoT Third App. All rights reserved
     </h3>
     </>
--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Header() {
   return (
     <>
-    <h1 className="text-center text-2xl from-red-500 to-blue-700 bg-linear-to-r bg-clip-text text-transparent font-bold mt-20">
+    <h1 className="text-center text-2xl from-red-700 to-blue-900 bg-linear-to-r bg-clip-text text-transparent font-bold mt-20">
       South Asian University EngComp 
     </h1>
     <hr className="mb-10"/> 
