d0f975ada095beaf562e3abba87de5bb17b2b046
DevCodingPhiJa <phijadev@gmail.com>
2026-02-03 21:05:21 +0700

    next-iot-third-app

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <>
     <h1 className="text-center text-2xl from-red-700 to-blue-900 bg-linear-to-r bg-clip-text text-transparent font-bold mt-20">
-      South Asian University EngComp 
+      South Asian University EngCom 
     </h1>
     <hr className="mb-10"/> 
     </>
