b30084f2d3fc814063fcd59b438333fdecbfef92
PhimahaDev <ophijagunchoself@gmail.com>
2026-02-07 12:09:01 +0700

    next-iot-third-app

--- app/page.tsx
+++ app/page.tsx
@@ -6,18 +6,14 @@ import Link from "next/link";
 export default function Page() {
   return (
     <>
-      <Image src={bmiImg} alt="BMI"
-             width={200} height={200}
-             className="mx-auto"/> 
+      <Image src={bmiImg} alt="BMI" width={200} height={200} className="mx-auto"/> 
       <h1 className="text-3xl text-center font-bold mt-4 text-blue-800"> 
         BMI CALCULATOR APP 
       </h1>
       <h3 className="text-center text-xl text-gray-600 mt-2 font-bold">
         Calculate Of BMI
       </h3>
-      <Link href="/bmi" className="w-50 bg-yellow-400 block mx-auto
-                  text-center text-black font-bold py-2 mt-4 mb-4 rounded
-                  hover:bg-red-600 transition">
+      <Link href="/bmi" className="w-50 bg-yellow-400 block mx-auto text-center text-black font-bold py-2 mt-4 mb-4 rounded hover:bg-red-600 transition">
         Go to BMI Calculator
       </Link> 
     </> 
